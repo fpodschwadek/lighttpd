fac71ae05cf0049e3fd7f3e4e2c78f9852a4b5f7
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2023-08-19 07:29:01 +0200

    Add log permissions adjustment and some inline comment to shell script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# If the log folder is mounted from the host, it might initially have a
+# different owner than www-data.
+chown -R www-data:www-data /var/log/lighttpd
+
+# Display error log entries via Docker.
 tail -f /var/log/lighttpd/error.log &
+
+# Start server.
 lighttpd -D -f /etc/lighttpd/lighttpd.conf
